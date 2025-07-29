ab59166f9c59485c2273c4717f7a092d96b490d1
Donald Kapyouo <donald-steve.kapyouo@bitmarck.de>
2025-07-29 20:30:35 +0200

    exercise 1: fix after video

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -5,7 +5,7 @@ function TextInput() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(guess);
+    console.log({ guess });
     setGuess('');
   }
   return (
@@ -16,6 +16,10 @@ function TextInput() {
         type="text"
         value={guess}
         pattern="[A-Z]{5}"
+        minLength={5}
+        maxLength={5}
+        title="Please enter a word of 5 letters"
+        required
         onChange={(event) => setGuess(event.target.value.toUpperCase())}
       />
     </form>
